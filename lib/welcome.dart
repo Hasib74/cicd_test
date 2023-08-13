f56ca1a13086efebb63f4ab0615295989d82fa55
Hasib74 <hasibakon74@gmail.com>
2023-08-13 16:09:56 +0600

    CICD test

---
+++ lib/welcome.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.width,
+        maxWidth: MediaQuery.of(context).size.width,
+      ),
+      child: const Row(
+        //direction: Axis.horizontal,
+        children: [
+          Text("Welcome"),
+          Icon(Icons.ac_unit),
+          Text("To bangladesh"),
+        ],
+      ),
+    );
+  }
+}
